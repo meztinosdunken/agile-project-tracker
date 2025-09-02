fbcf8af4b473507540412cbc917a16f75298ee34
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-02 11:28:55 -0500

    Fix typo in comment for clarity

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Basic route
+// Basic route for root endpoint
 app.get('/', (req, res) => {
   res.send('Welcome to the Agile Project Tracker API!');
 });
