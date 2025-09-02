439035ca0bb950223e1b27103aad541191577447
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-02 11:29:12 -0500

    Add middleware to serve static files from 'public' directory

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Basic route for root endpoint
 app.get('/', (req, res) => {
